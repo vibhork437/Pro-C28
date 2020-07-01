0dfa5fde620ddf2e0392b8ffc26ea3dc743d17d6
vibhork437 <65237757+vibhork437@users.noreply.github.com>
2020-07-01 18:50:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+const Constraint = Matter.Constraint
+
+var engine,world
+var ball
+var street
+
+function setup() {
+ canvas = createCanvas(1200,800);
+ engine =   Engine.create();
+ world = engine.world;
+
+street = loadImage("s.jpg") 
+ paper = new Paper(200,740,40);
+ ground = new Ground(750,750,1700,20);
+ground2 = new Ground(670,640,20,200)
+ground3 = new Ground(830,640,20,200)
+dustbin1 = new Dustbin(750,640,20,10)
+ 
+launcher = new Launcher(paper.body,{x:250,y:250});
+
+}
+
+function draw(){
+    background(street);
+    Engine.update(engine);
+    rectMode(CENTER);
+    paper.display();
+    ground.display();
+    ground2.display();
+    ground3.display();
+    dustbin1.display();
+    launcher.display();
+}
+/*
+function keyPressed(){
+if(keyCode === UP_ARROW){
+    Matter.Body.applyForce(paper.body,paper.body.position,{x:215,y:-235});
+}
+
+}
+*/
+
+function mouseDragged(){
+    Matter.Body.setPosition(paper.body,{x:mouseX,y:mouseY});
+};
+
+function mouseReleased(){
+    launcher.fly()
+}
